1043a6bc05f669879b38e46d3879f652a4ef0f8a
john <johnwbowker@gmail.com>
2021-03-25 21:01:27 -0400

    typo in start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-eudo docker run -d --rm -p 2456:2456/udp -p 2457:2457/udp -p 2458:2458/udp -v ~/valheim_worlds:"/root/.config/unity3d/IronGate/Valheim/worlds" valheim > container_id
+sudo docker run -d --rm -p 2456:2456/udp -p 2457:2457/udp -p 2458:2458/udp -v ~/valheim_worlds:"/root/.config/unity3d/IronGate/Valheim/worlds" valheim > container_id
