9ed8893d4515b0b527c06d97c6a437f6c1907a68
john <johnwbowker@gmail.com>
2021-08-16 17:17:11 -0400

    parameterized worldname and password

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-sudo docker run -d --rm --privileged -p 2456:2456/udp -p 2457:2457/udp -p 2458:2458/udp -v ~/valheim_data:"/root/.config/unity3d/IronGate/Valheim" valheim > container_id
+sudo docker run -d --rm  --privileged -p 2456:2456/udp -p 2457:2457/udp -p 2458:2458/udp -v ~/valheim_data:"/root/.config/unity3d/IronGate/Valheim" -e WORLD_NAME=Vagstabbin -e PASSWORD=lemonisagod valheim > container_id
